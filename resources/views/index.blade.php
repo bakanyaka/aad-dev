265d4351172d40e748b388426b9c276e1fa2bed7
Bakanyaka <bakayaroshka@gmail.com>
2017-04-16 15:50:15 +0300

    Basic Vue.JS setup. Fix Adldap error when no server available

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Arsenal Admin Dashboard</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="{{mix('/css/app.css')}}">
+    </head>
+    <body>
+        <div id="app">
+            <app></app>
+        </div>
+        <script>
+            window.Laravel = {!! json_encode([
+                'csrfToken' => csrf_token(),
+            ]) !!};
+        </script>
+        <script src="{{mix('/js/app.js')}}"></script>
+    </body>
+</html>
