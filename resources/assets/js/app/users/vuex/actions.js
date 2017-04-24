51871749dfbddb08a89ff9788ebbe09b6ea62c44
bakanyaka <bakayaroshka@gmail.com>
2017-04-24 17:03:08 +0300

    FrontEnd: Add sorting to filterUsers action

--- resources/assets/js/app/users/vuex/actions.js
+++ resources/assets/js/app/users/vuex/actions.js
@@ -1,4 +1,4 @@
-import {filter} from 'lodash'
+import {filter, sortBy} from 'lodash'
 export const fetchUsers = ({commit}) => {
     return axios.get('/api/users').then((response) => {
         commit('setUsers', response.data.data);
@@ -6,16 +6,15 @@ export const fetchUsers = ({commit}) => {
 };
 
 export const filterUsers = ({commit, state}, searchString) => {
-    commit('setFilteredUsers',
-        filter(state.users, (user) => {
-            for (let property in user) {
-                if (typeof user[property] === "string" && user[property].toLowerCase().includes(searchString.toLowerCase()) ) {
-                    return true
-                }
+    let filteredUsers = filter(state.users, (user) => {
+        for (let property in user) {
+            if (typeof user[property] === "string" && user[property].toLowerCase().includes(searchString.toLowerCase()) ) {
+                return true
             }
-            return false
-        })
-    )
+        }
+        return false
+    });
+    commit('setFilteredUsers', sortBy(filteredUsers, ['lastName', 'firstName']));
 };
 
 export const fetchUserDetails = ({commit}, user) => {
